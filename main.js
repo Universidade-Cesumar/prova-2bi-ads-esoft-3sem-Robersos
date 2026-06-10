875060845167c68d079fb10b766244b93aa511f7
Roberto Muller <betomullersanches@gmail.com>
2026-06-10 19:46:18 -0300

    cria variaveis do js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// Arquivo para código javascript
+const nomeItem = document.getElementById('input-nome');
+
+const qtdItem = document.getElementById('input-quantidade');
+
+const btnCadastro = document.getElementById('btn-cadastrar');
+
+const listaEstoque = document.getElementById('lista-estoque');
