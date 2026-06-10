11de9a563eb6ce6048e41822138e142ae79531d8
Roberto Muller <betomullersanches@gmail.com>
2026-06-10 19:59:41 -0300

    cria função para acesso a API

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
-const nomeItem = document.getElementById('input-nome');
+const URL_API = "https://6a29e985f59cb8f65f1dc4af.mockapi.io/almx/item";
+const nomeItemInput = document.getElementById('input-nome');
 
-const qtdItem = document.getElementById('input-quantidade');
+const qtdItemInput = document.getElementById('input-quantidade');
 
 const btnCadastro = document.getElementById('btn-cadastrar');
 
 const listaEstoque = document.getElementById('lista-estoque');
+
+btnCadastro.addEventListener('click', function() {
+    const nomeItem = nomeItemInput.value;
+    const qtdItem = parseInt(qtdItemInput.value);
+
+    
+    if (!nomeItem || isNaN(qtdItem)) {
+        alert("Preencha todos os campos corretamente!");
+        return;
+    }
+
+    const novoProduto = {
+        nome: nomeItem,
+        quantidade: qtdItem
+    };
+
+  
+    fetch(URL_API, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novoProduto)
+    })
+    .then(resposta => resposta.json())
+    .then(dadosSalvos => {
+        console.log("Item cadastrado com sucesso:", dadosSalvos);
+        
+        
+        inputNome.value = "";
+        inputQtd.value = "";
+
+        atualizarTelaAlmoxarifado();
+    })
+    .catch(erro => console.error("Erro ao cadastrar:", erro));
+});
